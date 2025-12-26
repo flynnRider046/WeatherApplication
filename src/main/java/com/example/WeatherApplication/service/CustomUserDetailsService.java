4644cea08456638d2d7c0cd81462887985cc4e1c
flynnRider046 <rohithdamgeti@gmail.com>
2025-12-26 16:13:21 -0600

    Adding Log4j2 | Introduction to Splunk

--- src/main/java/com/example/WeatherApplication/service/CustomUserDetailsService.java
+++ src/main/java/com/example/WeatherApplication/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.example.WeatherApplication.service;
 
 import com.example.WeatherApplication.repository.UserDetailsRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,11 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
 
+
+    Logger logger = LogManager.getLogger(CustomUserDetailsService.class);
+
     @Autowired
     private UserDetailsRepository userDetailsRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Load By UserName: Execution started");
         return userDetailsRepository.findByUserName(username).orElseThrow(() ->
                 new UsernameNotFoundException("Username not Found"));
     }
